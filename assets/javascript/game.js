49f780f1de10ba606273569cb5b049e086d2815f
jharnois444 <jharnois444@gmail.com>
2018-03-09 08:54:27 -0500

    new commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,15 @@
-var targetScore = Math.floor(Math.random() * 120 - 19 + 1) + 19;
-$("#targetScore").text("targetScore");
+// var targetScore = Math.floor(Math.random() * 120 - 19 + 1) + 19;
+// $("#targetScore").text("targetScore");
+
+
+
+  // Here we created an on-click event that responds to button clicks of the crystal image.
+  $("#crystalRow").on("click", ".thumbnail", function() {
+
+    // Clicking the button triggers an alert message.
+    alert("You clicked a crystal!");
+
+  });
+
+
+
